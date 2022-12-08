abd9bff812a3b6f3e5cd77a6878476120c59f336
AlamoGaston <alamolionel@live.com.ar>
2022-12-07 23:21:47 -0300

    1st change

--- daos/carts/cartDAOFile.js
+++ daos/carts/cartDAOFile.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const file = require("../../data/carts.txt");
+const file = require("../../data/cart.json");
 const CrudFile = require("../../data/CRUDTxt/crudCarts");
 
 class CartDAOFile extends CrudFile {
--- daos/products/productsDAOFile.js
+++ daos/products/productsDAOFile.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const file = require("../../data/products.txt");
+const file = require("../../data/products.json");
 const CRUDFile = require("../../data/CRUDTxt/crudProducts");
 
 class ProductsDAOFile extends CRUDFile {
